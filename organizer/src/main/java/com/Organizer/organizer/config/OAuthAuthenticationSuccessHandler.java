fcdd049d1f3d3ea11a3ea14d630ac81170963e50
Nameet Karadi <nameetsk1@gmail.com>
2024-07-31 14:38:44 +0530

    Login changes

--- organizer/src/main/java/com/Organizer/organizer/config/OAuthAuthenticationSuccessHandler.java
+++ organizer/src/main/java/com/Organizer/organizer/config/OAuthAuthenticationSuccessHandler.java
@@ -1,26 +1,26 @@
-package com.Organizer.organizer.config;
+// package com.Organizer.organizer.config;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-import org.hibernate.annotations.Comment;
-import org.hibernate.validator.internal.util.logging.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
+// import org.hibernate.annotations.Comment;
+// import org.hibernate.validator.internal.util.logging.LoggerFactory;
+// import org.springframework.security.core.Authentication;
+// import org.springframework.security.web.DefaultRedirectStrategy;
+// import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+// import org.springframework.stereotype.Component;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
 
-@Component
-public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessHandler{
+// @Component
+// public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessHandler{
 
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-            Authentication authentication) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        new DefaultRedirectStrategy().sendRedirect(request, response,"/user/");
-        // throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
-    }
-}
+//     @Override
+//     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+//             Authentication authentication) throws IOException, ServletException {
+//         // TODO Auto-generated method stub
+//         new DefaultRedirectStrategy().sendRedirect(request, response,"/user/");
+//         // throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
+//     }
+// }
--- organizer/src/main/java/com/Organizer/organizer/config/securityConfig.java
+++ organizer/src/main/java/com/Organizer/organizer/config/securityConfig.java
@@ -5,29 +5,20 @@ import java.beans.Customizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import com.Organizer.organizer.services.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class securityConfig  {
-    @Autowired
-    private OAuthAuthenticationSuccessHandler Handler;
+
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
@@ -114,11 +105,7 @@ public class securityConfig  {
         logutP.logoutSuccessUrl("/login?logout=true");
     });
 
-    //auth configuration for google
-    httpSecurity.oauth2Login(oauth->{
-        oauth.loginPage("/login");
-        oauth.successHandler(Handler);
-    });
+
     return httpSecurity.build();
 
     }
